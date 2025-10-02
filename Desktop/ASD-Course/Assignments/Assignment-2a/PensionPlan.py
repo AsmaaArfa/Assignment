5873d78158711c97aa94330416ff9f2677610d7a
AsmaaArfa <aarfa@miu.edu>
2025-10-02 10:50:12 -0500

    Delete Desktop/ASD-Course/Assignments directory

--- Desktop/ASD-Course/Assignments/Assignment-2a/PensionPlan.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field, field_validator
-import pandas as pd
-
-class PensionPlan(BaseModel):
-    P_Ref_Num: str 
-    enrollmentDate: str |None = Field(None)
-    monthlyContribution: int = Field(...)
