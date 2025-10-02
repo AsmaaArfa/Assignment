1d706e152b37518c61326fcf669917bad64aa4e3
asmaahesham <aarfa@miu.edu>
2025-10-02 10:06:03 -0500

    change setup

--- Desktop/ASD-Course/Assignment_lab/Assignment-2a/setup.py
+++ Desktop/ASD-Course/Assignment_lab/Assignment-2a/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="employee-pensionplan",       # Your package name
-    version="0.1.2",                   # Version number
+    version="0.1.*",                   # Version number
     packages=find_packages(),          # Automatically find packages
     install_requires=[
         "pandas","pydantic"                      # Add other dependencies here
